2f8a0d1dce0259736f7c164d6504bec5ab633944
Nkambi Julius Forsuh <nkambijuliusforsuh@gmail.com>
2026-09-17 14:31:18 +0100

    Little updates

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'top', // Forces scroll to top on navigation
+        anchorScrolling: 'enabled',       // Optional: helps support hash anchor links (#id)
+      })
+    )
   ]
 };
